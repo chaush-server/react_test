47a6b93387c67919dcc9a06f30b45e9d77b940f4
Server <chaush.s.i20@gmail.com>
2023-10-03 14:18:51 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import TodoList from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <TodoList/>
+);

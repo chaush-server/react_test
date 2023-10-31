e8694cec63b7db892dd4cfbe70da63fc984ce6c4
Server <chaush.s.i20@gmail.com>
2023-10-31 14:25:28 +0300

    5.1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import {useState} from 'react';
-import {Container} from 'react-bootstrap';
-import { CSSTransition } from 'react-transition-group';
-import './App.css';
+import { books } from "./data";
+import { Books } from "./components/Books";
 
-const Modal = (props) => {
-
-    const duration = 500;
-
-    return (
-        <CSSTransition
-            in={props.show}
-            timeout={duration}
-            onEnter={() => props.setShowTrigger(false)}
-            onExited={() => props.setShowTrigger(true)}
-            classNames="modal"
-            mountOnEnter
-            unmountOnExit
-            >
-                <div className="modal mt-5 d-block">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Typical modal window</h5>
-                        </div>
-                        <div className="modal-body">
-                            <p>Modal body content</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
-                            <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            
-        </CSSTransition>
-    )
-}
+import { useLocalStorage } from "./hooks/useLocalStorage.js";
 
 function App() {
-    const [showModal, setShowModal] = useState(false);
-    const [showTrigger, setShowTrigger] = useState(true);
+  const [order, setOrder] = useLocalStorage([], "order");
 
-    return (
-        <Container>
-            <Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger} />
-            {showTrigger ?
-            <button 
-                type="button" 
-                className="btn btn-warning mt-5"
-                onClick={() => setShowModal(true)}>Open Modal</button> :
-                null}
-        </Container>
-    );
+  const addToOrder = (id) => {
+    const newItem = books.find((item) => item.id === id);
+
+    setOrder([...order, newItem]);
+    console.log(order);
+  };
+
+  return <Books items={books} addToOrder={addToOrder} />;
 }
 
 export default App;

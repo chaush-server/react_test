334fa5ad07952af60721cf002206f96d7db29242
Server <chaush.s.i20@gmail.com>
2023-10-31 15:17:36 +0300

    t

--- src/confirm.js
+++ src/confirm.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import './App.css'
 const ConfirmationDialog = ({ onConfirm, onCancel }) => {
   return (
-    <div className="confirmation-dialog">
+    <div class="backdrop">
+      <div class='modal'>
       <p>Вы действительно хотите удалить фигуру?</p>
       <button onClick={onConfirm}>Да</button>
       <button onClick={onCancel}>Отмена</button>
+      </div>
     </div>
   );
 };

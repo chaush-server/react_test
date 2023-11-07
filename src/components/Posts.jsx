abd9eca0e3b95fb36fe98b788d02ac8db4d9c64d
Server <chaush.s.i20@gmail.com>
2023-11-07 14:34:18 +0300

    5.4

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,90 +2,85 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Posts() {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        setPosts(response.data);
-      })
-      .catch((error) => {
-        console.error('Ошибка при загрузке данных:', error);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                setPosts(response.data);
+            });
+    }, []);
 
-  return (
-    <div>
-      <h1>Список постов и комментариев</h1>
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <h1>ПОСТЫ:</h1>
+            {posts.map((post) => (
+                <Post key={post.id} post={post} />
+            ))}
+        </div>
+    );
 };
 
 function Post({ post }) {
     const [newComment, setNewComment] = useState({ name: '', text: '' });
     const [comments, setComments] = useState([]);
-  
+
     const handleCommentSubmit = () => {
-      // Добавляем новый комментарий в список комментариев на странице
-      setComments([...comments, newComment]);
-      // Сбрасываем форму
-      setNewComment({ name: '', text: '' });
+        setComments([...comments, newComment]);
+        setNewComment({ name: '', text: '' });
     };
-  
+
     return (
-      <div>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-        <Comments comments={comments} />
-        <CommentForm
-          newComment={newComment}
-          onCommentChange={(e) =>
-            setNewComment({ ...newComment, [e.target.name]: e.target.value })
-          }
-          onCommentSubmit={handleCommentSubmit}
-        />
-      </div>
+        <div>
+            <h1 style={{color: 'green'}}>{'title: ' + post.title}</h1>
+            <h2 style={{color: 'orange'}}>{'body: ' + post.body}</h2>
+            <Comments comments={comments} />
+            <CommentForm
+                newComment={newComment}
+                onCommentChange={(e) =>
+                    setNewComment({ ...newComment, [e.target.name]: e.target.value })
+                }
+                onCommentSubmit={handleCommentSubmit}
+            />
+        </div>
     );
-  }
-  
-  function Comments({ comments }) {
+}
+
+function Comments({ comments }) {
     return (
-      <div>
-        <h3>Комментарии</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>
-              {comment.name}: {comment.text}
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div>
+            <div>Комментарии</div>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>
+                        {comment.name}: {comment.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  }
-  
-  function CommentForm({ newComment, onCommentChange, onCommentSubmit }) {
+}
+
+function CommentForm({ newComment, onCommentChange, onCommentSubmit }) {
     return (
-      <div>
-        <h3>Добавить комментарий</h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Ваше имя"
-          value={newComment.name}
-          onChange={onCommentChange}
-        />
-        <textarea
-          name="text"
-          placeholder="Текст комментария"
-          value={newComment.text}
-          onChange={onCommentChange}
-        />
-        <button onClick={onCommentSubmit}>Отправить</button>
-      </div>
+        <div>
+            <div>Добавить комментарий</div>
+            <input
+                type="text"
+                name="name"
+                placeholder="Ваше имя"
+                value={newComment.name}
+                onChange={onCommentChange}
+            />
+            <textarea
+                name="text"
+                placeholder="Текст комментария"
+                value={newComment.text}
+                onChange={onCommentChange}
+            />
+            <button onClick={onCommentSubmit}>Отправить</button>
+        </div>
     );
-  }
-  
+}
+
 export default Posts;
